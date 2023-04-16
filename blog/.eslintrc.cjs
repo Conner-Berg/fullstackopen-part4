9ebdf253bf2db66eb554c53339de643225ff53bc
Conner-Berg <connerwberg@gmail.com>
2023-04-16 12:23:39 -0400

    Create eslint config file

---
+++ blog/.eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ["eslint:recommended", "plugin:react/recommended"],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	settings: {
+		react: {
+			version: "^18.2.0",
+		},
+	},
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		eqeqeq: "error",
+		"no-trailing-spaces": "error",
+		"object-curly-spacing": ["error", "always"],
+		"arrow-spacing": ["error", { before: true, after: true }],
+		"no-console": 0,
+	},
+};
